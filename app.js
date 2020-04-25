508dcbb36216e5ded25194656fb774fd2afeb5f1
memeghaj <meghajsingh13@hotmail.com>
2020-04-25 20:18:57 +0000

    port change

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() {
+app.listen(8000, function() {
 	console.log("YelpCamp has started");
 });
