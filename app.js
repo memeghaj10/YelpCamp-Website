c6405f66f377302d27949dfe371339b06669482e
memeghaj <meghajsingh13@hotmail.com>
2020-04-25 19:38:32 +0000

    add mongolab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://yelpcamp:yelpcamp@yelpcamp-oueaa.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
